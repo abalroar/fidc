28cc868b915c7c6487c284f359d44dcd2ef20616
abalroar <matheusjprates@gmail.com>
2026-01-31 16:45:58 -0300

    Simplify Streamlit upload app

--- fidc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""FIDC amortization app package."""
-
-from fidc.excel import ExcelInputs, load_excel_inputs
-from fidc.model import ModelInputs, ModelOutputs, run_model
-
-__all__ = ["ExcelInputs", "load_excel_inputs", "ModelInputs", "ModelOutputs", "run_model"]
